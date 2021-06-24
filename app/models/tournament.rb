9b50862bb1c2b3ccf86b91da495f9f7fb84a9ef2
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-24 09:20:15 -0400

    model fix relationships, one join table

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,4 @@
 class Tournament < ApplicationRecord
-    has_many :team_tournaments
-    has_many :teams, through: :team_tournaments
-    has_many :games, through: :teams
+    has_many :games
+    has_many :teams, through: :games
 end
