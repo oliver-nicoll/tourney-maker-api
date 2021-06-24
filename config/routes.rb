a94264cb2500123bc28a3eb25630a0d648b6d503
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-24 09:21:21 -0400

    remove route not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   # resources :tournaments
-  # resources :team_tournaments
   # resources :games
   # resources :teams
 
   namespace :api do
     namespace :v1 do
       resources :tournaments, only: [:index, :show, :create]
-      resources :teams, only: [:show, :create]
+      resources :teams, only: [:index, :show, :create]
       resources :games
-      resources :team_tournaments
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
