300f32609c3818c4a706a8409973402bbbc43a48
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-24 09:19:09 -0400

    combine and fix into one join table

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,4 @@
 class Team < ApplicationRecord
-    has_many :team_tournaments
-    has_many :tournaments, through: :team_tournaments
     has_many :games
+    has_many :tournaments, through: :games
 end
