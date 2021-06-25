4f2c3a1b49109f825d9949af3f1c745929251bc2
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-25 13:35:35 -0400

    teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,13 @@
 class Team < ApplicationRecord
-    has_many :games
-    has_many :tournaments, through: :games
+    has_many :registrations
+    has_many :tournaments, through: :registrations
+
+    validates :team_name, presence: true
+
+
+    def shuffle_teams(id) 
+        @teamid = @teams.find_by(team_id: team.id) 
+        @shuffle_teams = @teams.shuffle
+        #tournament.teams relationship
+    end
 end
