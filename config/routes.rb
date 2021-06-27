3111224aadba0490a2a14c578fd6014dae068aa7
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-27 01:37:43 -0400

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tournaments do
-        resources :registrations, only: [:create, :destroy] 
+        resources :registrations, only: [:create, :show, :destroy] 
       end
       resources :teams
     end
